1ed1e55b698642f01de80e6147733d13ba5f0fa6
Kauaro <kauaro1103@gmail.com>
2025-08-07 20:04:53 -0300

    Add project files.

---
+++ ProductClientHub.API/Infrastructure/ProductClientHubDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infrastructure
+{
+    public class ProductClientHubDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; } = default!;
+        public DbSet<Product> Products { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Projetos\\C#\\Curso\\ProductClientHub.db.octet-stream");
+        }
+    }
+}
